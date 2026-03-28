1c6b2ebe62b534774eabb428999b568d83cd15da
wigya <amajewski505@gmail.com>
2026-03-28 17:19:03 +0100

    feat(Utils): add RentalConstants in utils

---
+++ ConsoleApp1/Utils/RentalConstants.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp1.Utils;
+
+public class RentalConstants
+{
+    public const int MaxStudentRentals = 2;
+    public const int MaxEmployeeRentals = 5;
+
+    public const decimal PenaltyForEachDayDelay = 40;
+}
